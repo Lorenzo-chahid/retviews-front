cc7d963d104d28e4dbe3a439c48b7d5fed7fdb5e
lorenzo chahid <clkwebdeveloper@gmail.com>
2024-02-13 17:56:18 +0100

    fix issues route new

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth.service';
+import { ClothingService, ClothingItem } from '../clothing.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,20 +8,26 @@ import { AuthService } from '../auth.service';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-  user: any;
+  user: any; // Typez correctement en fonction de votre application
+  lastAddedItems: ClothingItem[] = [];
 
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private clothingService: ClothingService) { }
 
-  ngOnInit(): void {
-    const userData = localStorage.getItem('user');
-    console.log("USER :: ",userData);
-    if (userData) {
-      this.user = JSON.parse(userData);
-      console.log("USER DATA : ",this.user)
-    }
+  ngOnInit() {
+    this.getUserInfo();
+    this.getLastAddedItems();
   }
 
-  logout(): void {
-    this.authService.logout();
+  getUserInfo() {
+    // Vous aurez besoin d'implémenter une méthode dans AuthService pour récupérer les infos de l'utilisateur
+    // this.authService.getUserInfo().subscribe(user => this.user = user);
+  }
+
+  getLastAddedItems() {
+    this.clothingService.getClothingItems().subscribe(items => {
+      // Vous devrez filtrer ou ajuster cette logique en fonction de la réponse de votre API
+      // Pour cet exemple, je suppose que vous avez déjà le dernier article par catégorie
+      this.lastAddedItems = items;
+    });
   }
 }
